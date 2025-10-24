2604a50ffb52cc779c7386b3f36ed815f3cb2834
Tony Shangkuan @ desktop <tonyshangkuan@gmail.com>
2025-10-24 20:25:20 +0800

    complete docker-based gemma3 model

---
+++ DockerModelDemo/src/main/java/com/example/DockerModelDemo/DockerModelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerModelDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerModelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerModelDemoApplication.class, args);
+	}
+
+}
---
+++ DockerModelDemo/src/test/java/com/example/DockerModelDemo/DockerModelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerModelDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerModelDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
