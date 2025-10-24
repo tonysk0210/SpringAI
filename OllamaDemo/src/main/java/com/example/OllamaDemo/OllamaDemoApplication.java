94e597693f65998bd0343e77e3792fbcbcdfbb2e
Tony Shangkuan @ desktop <tonyshangkuan@gmail.com>
2025-10-24 17:03:17 +0800

    complete ollama demo

---
+++ OllamaDemo/src/main/java/com/example/OllamaDemo/OllamaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.OllamaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaDemoApplication.class, args);
+	}
+
+}
---
+++ OllamaDemo/src/test/java/com/example/OllamaDemo/OllamaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OllamaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
